dcc8cd1f228e95868f4b5b2c8cdcec1fa900bd58
Kevin Washington <87237986+KevinWashington@users.noreply.github.com>
2021-11-15 04:15:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
-let seconds = 7
-let minutes = 0
-let minutesInt = 0
-let secondsInt = 5
+let seconds = 0
+let minutes = 25
+let minutesInt = 5
+let secondsInt = 0
+let minuteslongbreak = 14
+let secondslongbreak = 60
+let longbreaktime = 4
 let cycles = 8
 let cont = 1
-let contclicks = 0
-let minuteslongbreak = 0
-let secondslongbreak = 20
-let longbreaktime = 4
+let circuloAnimado = document.querySelector(".circulo-animado")
+let inicial = "440"
 
 function start() {
   //Play notification when it starts
   document.querySelector('#notification').play()
+  document.querySelector('#start').setAttribute('disabled', 'disabled')
   cont = 0 //Reset cycle counter
-  contclicks++ //Click counter
-  document.querySelector('#start').value = 'reset'
-  //If more than one click is given on start, the time will be reset
-  if (contclicks > 1) {
-    clearTimeout(cron)
-    seconds = 0
-    minutes = 25
-    minutesInt = 4
-    secondsInt = 60
-    timer.innerHTML = `${minutes}:0${seconds}`
-  }
   //Activate the counter function every second.
   cron = setInterval(() => counter(), 1000)
+}
+
+function reset() {
+  clearTimeout(cron)
+  inicial = "440"
+  seconds = 0
+  minutes = 25
+  minutesInt = 4
+  secondsInt = 60
+  minuteslongbreak = 14
+  secondslongbreak = 60
+  timer.innerHTML = `${minutes}:0${seconds}`
+  document.querySelector('#start').removeAttribute('disabled', 'disabled')
 }
 
 //Time pause function
 function pause() {
   clearInterval(cron)
+  document.querySelector('#start').removeAttribute('disabled', 'disabled')
 }
 
 //Function that will be activated each second.
@@ -38,53 +43,56 @@ function counter() {
   //Check if the number of repetitions is equal to the number of cycles
   if (cont <= cycles) {
     //If the normal time ends, break starts
-    if (minutes == 0 && seconds == 00) {
-      if (cont == longbreaktime || cont == longbreaktime*2) {
+    if (minutes == 0 && seconds == 0) {
+      
+      if (cont == longbreaktime) {
         longbreak()
         if (minuteslongbreak == 0 && secondslongbreak == 0) {
           document.querySelector('#notification').play()
-          minutes = 0
-          seconds = 7
-          minutesInt = 0
-          secondsInt = 5
-          minuteslongbreak = 0
-          secondslongbreak = 20
+          inicial = "440"
+          seconds = 60
+          minutes = 24
+          minuteslongbreak = 14
+          secondslongbreak = 60
           cont++ //End a cycle
         }
       } else {
         shortbreak()
         //If the break ends, normal time starts
         if (minutesInt == 0 && secondsInt == 0) {
+          inicial = "440"
           document.querySelector('#notification').play()
-          minutes = 0
-          seconds = 7
-          minutesInt = 0
-          secondsInt = 5
+          seconds = 60
+          minutes = 24
+          minutesInt = 4
+          secondsInt = 60
           cont++ //End a cycle
         }
       }
     } else {
       //Until time runs out, normal time continues
       normaltimer()
-      //When normal time runs out, alarm sounds
-      if (minutes == 0 && seconds == 00) {
+      if (minutes == 0 && seconds == 0) {
+        inicial = "440"
         document.querySelector('#notification').play()
       }
     }
   } else {
     //If the number of repetitions is equal to the number of cycles
     clearInterval(cron)
-    seconds = 60
-    minutes = 24
-    document.querySelector('h1#header').innerHTML = 'Great Job!'
-    let timer = document.querySelector('#timer')
-    timer.innerHTML = 'The cycles are over'
+    document.querySelector('#text').innerHTML = 'Great Job!'
+
+    document.querySelector('#timer').innerHTML = 'The cycles are over'
   }
 }
 
 function normaltimer() {
-  document.querySelector('h1#header').innerText = 'Stay focus!'
-  let timer = document.querySelector('#timer')
+
+  document.querySelector(".circulo-animado").style.strokeDashoffset = inicial-(inicial/(minutes*60+seconds))
+  inicial = inicial-(inicial/(minutes*60+seconds))
+
+  document.querySelector('#text').innerText = 'Focus time!'
+
   //When 60 seconds pass, add minute.
   if (seconds == 0) {
     minutes--
@@ -96,45 +104,41 @@ function normaltimer() {
   seconds--
   seconds < 10 ? (seconds = '0' + seconds) : (seconds = seconds)
   //Refresh the counter every second
-  timer.innerHTML = `${minutes}:${seconds}`
+  document.querySelector('#timer').innerHTML = `${minutes}:${seconds}`
 }
 
 function shortbreak() {
-  document.querySelector('h1#header').innerText = 'Take a break'
-  let timer = document.querySelector('#timer')
+  document.querySelector(".circulo-animado").style.strokeDashoffset = inicial-(inicial/(minutesInt*60+secondsInt))
+  inicial = inicial-(inicial/(minutesInt*60+secondsInt))
+  document.querySelector('#text').innerText = 'Break Time'
   //When 60 seconds pass, add minute.
   if (secondsInt == 0) {
     minutesInt--
     secondsInt = 60
     //2-digit checker
-    minutesInt < 10
-      ? (minutesInt = '0' + minutesInt)
-      : (minutesInt = minutesInt)
+    minutesInt < 10 ? (minutesInt = '0' + minutesInt) : (minutesInt = minutesInt)
   }
   //2-digit checker
   secondsInt--
   secondsInt < 10 ? (secondsInt = '0' + secondsInt) : (secondsInt = secondsInt)
   //Refresh the counter every second
-  timer.innerHTML = `${minutesInt}:${secondsInt}`
+  document.querySelector('#timer').innerHTML = `${minutesInt}:${secondsInt}`
 }
 
 function longbreak() {
-  document.querySelector('h1#header').innerText = 'Take a long break'
-  let timer = document.querySelector('#timer')
+  document.querySelector(".circulo-animado").style.strokeDashoffset = inicial-(inicial/(minuteslongbreak*60+secondslongbreak))
+  inicial = inicial-(inicial/(minuteslongbreak*60+secondslongbreak))
+  document.querySelector('#text').innerText = 'Long break Time'
   //When 60 seconds pass, add minute.
   if (secondslongbreak == 0) {
     minuteslongbreak--
     secondslongbreak = 60
     //2-digit checker
-    minuteslongbreak < 10
-      ? (minuteslongbreak = '0' + minuteslongbreak)
-      : (minuteslongbreak = minuteslongbreak)
+    minuteslongbreak < 10 ? (minuteslongbreak = '0' + minuteslongbreak) : (minuteslongbreak = minuteslongbreak)
   }
   //2-digit checker
   secondslongbreak--
-  secondslongbreak < 10
-    ? (secondslongbreak = '0' + secondslongbreak)
-    : (secondslongbreak = secondslongbreak)
+  secondslongbreak < 10 ? (secondslongbreak = '0' + secondslongbreak) : (secondslongbreak = secondslongbreak)
   //Refresh the counter every second
-  timer.innerHTML = `${minuteslongbreak}:${secondslongbreak}`
+  document.querySelector('#timer').innerHTML = `${minuteslongbreak}:${secondslongbreak}`
 }
